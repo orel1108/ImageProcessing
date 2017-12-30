4d3a42138552262829c72b330bf5eeb4dc85f913
Roman Orlovskyi <r.orlovskyi@gmail.com>
2017-12-30 16:14:11 +0200

    Add comments ImageIO

--- ImageProcessing/ImageIO.h
+++ ImageProcessing/ImageIO.h
@@ -3,15 +3,37 @@
 #include <QImage>
 #include <QString>
 
+/**
+ * class ImageIO
+ * @brief Class implements image load / save operations.
+ */
 class ImageIO
   {
   public:
+    /**
+     * @brief Getter for image.
+     * @return Loaded image.
+     */
     const QImage& Get() const;
 
+    /**
+     * @brief Sets image.
+     * @param[in] i_image Image to be set.
+     */
     void Set(const QImage& i_image);
 
+    /**
+     * @brief Loads image from file.
+     * @param[in] i_filename Name of file with image.
+     * @return true on operation success and false otherwise.
+     */
     bool Load(const QString& i_filename);
     
+    /**
+     * @brief Saves image to file.
+     * @param[in] i_filename Name of file.
+     * @return true on operation success and false otherwise.
+     */
     bool Save(const QString& i_filename) const;
 
   private:
