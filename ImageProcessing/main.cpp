c0b6bde762c7b08c90fceeb364e9fad7feacba9b
Roman Orlovskyi <r.orlovskyi@gmail.com>
2018-03-31 18:03:06 +0300

    Delete all code

--- ImageProcessing/main.cpp
+++ ImageProcessing/main.cpp
@@ -1,11 +1,7 @@
-#include <ImageProcessing.h>
-
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ImageProcessing w;
-    w.show();
     return a.exec();
 }
